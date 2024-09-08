c31a41889f3f0dcc2b286f4c447a3249fa3aac5c
LokiGitHubb <loki2020poop@gmail.com>
2024-09-07 17:45:03 -0700

    work

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       jding                                                        #
+# 	Created:      9/7/2024, 5:42:49 PM                                         #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
